305529eb10fb143389e6709c58ec9cd7fbfe0db1
Júlio Freitas <jcsr.frts@gmail.com>
2023-11-23 14:58:18 -0300

    Tarefa Gulp para construir e minificar a saída CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
-const gulp = require("gulp");
+import gulp from "gulp";
+import sass from "gulp-sass";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const gulpSass = sass(require("sass"));
+
+const SCSS_FILES_PATH = "./src/scss/*.scss";
+const CSS_OUTPUT_FILE_PATH = "./src/assets/css";
+
+function sassCompiler() {
+    return gulp.src(SCSS_FILES_PATH)
+        .pipe(gulpSass({ outputStyle: "compressed" }))
+        .pipe(gulp.dest(CSS_OUTPUT_FILE_PATH));
+}
+
+gulp.task('default', sassCompiler);
 
