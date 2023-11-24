6d6e46cf70a33d193903a7f8d3da01099d0324fa
Júlio Freitas <jcsr.frts@gmail.com>
2023-11-24 17:10:47 -0300

    Configurado Gulp auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 import gulp from "gulp";
+import autoPrefixer from "gulp-autoprefixer";
 import sass from "gulp-sass";
 import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const gulpSass = sass(require("sass"));
 
 const SCSS_FILES_PATH = "./src/scss/*.scss";
 const CSS_OUTPUT_FILE_PATH = "./src/assets/css";
 
+const require = createRequire(import.meta.url);
+const gulpSass = sass(require("sass"));
+
 function sassCompiler() {
     return gulp.src(SCSS_FILES_PATH)
         .pipe(gulpSass({ outputStyle: "compressed" }))
+        .pipe(autoPrefixer({
+            overrideBrowserslist: ["last 2 versions"],
+            cascade: false
+        }))
         .pipe(gulp.dest(CSS_OUTPUT_FILE_PATH));
 }
 
