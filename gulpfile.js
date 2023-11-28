b471c0b1172e04988b8d8d5ca01a860fc7e6b397
Júlio Freitas <jcsr.frts@gmail.com>
2023-11-28 14:28:49 -0300

    Configurado Gulp Concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,50 @@
 import browserSync from "browser-sync";
 import gulp from "gulp";
 import autoPrefixer from "gulp-autoprefixer";
+import concat from "gulp-concat";
 import sass from "gulp-sass";
 import { createRequire } from "module";
 
-const SCSS_FILES_PATH = "./src/scss/*.scss";
-const CSS_OUTPUT_FILE_PATH = "./src/assets/css";
+const WEBSITE_BASE_DIR = "./src";
 
 const require = createRequire(import.meta.url);
 const gulpSass = sass(require("sass"));
 const browserSyncInstance = browserSync.create();
 
 function sassCompiler() {
-    return gulp.src(SCSS_FILES_PATH)
+    return gulp.src(`${WEBSITE_BASE_DIR}/scss/*.scss`)
         .pipe(gulpSass({ outputStyle: "compressed" }))
         .pipe(autoPrefixer({
             overrideBrowserslist: ["last 2 versions"],
             cascade: false
         }))
-        .pipe(gulp.dest(CSS_OUTPUT_FILE_PATH))
+        .pipe(gulp.dest(`${WEBSITE_BASE_DIR}/assets/css`))
         .pipe(browserSyncInstance.stream());
 }
+gulp.task("sass", sassCompiler);
 
-gulp.task("default", sassCompiler);
+function scripts() {
+    return gulp.src('./src/assets/js/scripts/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./src/assets/js/scripts/dist/'));
+}
+gulp.task('scripts', scripts);
 
 function watch() {
-    gulp.watch(SCSS_FILES_PATH, sassCompiler);
-    gulp.watch("./src/*.html").on("change", browserSyncInstance.reload);
+    gulp.watch(`${WEBSITE_BASE_DIR}/scss/*.scss`, sassCompiler);
+    gulp.watch(`${WEBSITE_BASE_DIR}/*.html`).on("change", browserSyncInstance.reload);
+    gulp.watch(`${WEBSITE_BASE_DIR}/assets/js/scripts/*.js`, scripts);
 }
-
 gulp.task("watch", watch);
 
 function browser() {
     browserSyncInstance.init({
         server: {
-            baseDir: "./src/"
+            baseDir: WEBSITE_BASE_DIR
         }
     });
 }
 
 gulp.task("browser-sync", browser);
 
-gulp.task("default", gulp.parallel("watch", "browser-sync"));
+gulp.task("default", gulp.parallel("watch", "browser-sync", "sass", "scripts"));
---
+++ src/assets/js/scripts/dist/all.js
@@ -0,0 +1,6 @@
+{
+    console.log("buttonsjs");
+} 
+{
+    console.log("modaljs");
+}
