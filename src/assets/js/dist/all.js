27bfe9bb2a10181f47e9cb5044e38b65085d0dec
Júlio Freitas <jcsr.frts@gmail.com>
2023-12-06 14:41:37 -0300

    Hero pt 2

--- src/assets/js/dist/all.js
+++ src/assets/js/dist/all.js
@@ -1,1 +1,1 @@
-"use strict";var slide_hero=new Swiper("#main-slide",{effect:"fade"});
+"use strict";var slide_thumb=new Swiper("#thumb-slide",{slidesPerView:5,direction:"vertical",spaceBetween:20,watchSlidesProgress:!0}),slide_hero=new Swiper("#main-slide",{effect:"fade",thumbs:{swiper:slide_thumb},autoPlay:{delay:5e3,disableOnInteraction:!1}});
--- src/assets/js/scripts/main.js
+++ src/assets/js/scripts/main.js
@@ -1,3 +1,17 @@
+const slide_thumb = new Swiper("#thumb-slide", {
+    slidesPerView: 5,
+    direction: "vertical",
+    spaceBetween: 20,
+    watchSlidesProgress: true
+});
+
 const slide_hero = new Swiper("#main-slide", {
-    effect: "fade"
+    effect: "fade",
+    thumbs: {
+        swiper: slide_thumb
+    },
+    autoPlay: {
+        delay: 5000,
+        disableOnInteraction: false
+    }
 });
